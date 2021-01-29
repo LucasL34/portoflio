c383d99e82c1cbfa2551fa4db0829bb4ab8e4ba9
Lucas_ubuntu <34lucaslopez@gmail.com>
2021-01-29 16:58:56 -0300

    feat: add axios, change bootstrap class & minor fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Nav from './components/nav.js';
 import Buscador from './components/buscador.js';
 import Content from './components/content.js';
+import Footer from './components/footer'
 
 import './App.css';
 import './bootstrap.min.css';
 
 function App() {
+  //<div className="App"> 
   return (
-    <div className="App">
+    <div className="App"> 
       <Nav/>
       <Buscador/>
       <Content/>
+      <Footer/>
     </div>
   );
 }
--- src/components/content.js
+++ src/components/content.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import Card from './card.js';
 
 class Content extends Component {
 
-    
+    state = {
+        cards: []
+    };
 
+    url = "https://jsonplaceholder.typicode.com/todos";
+
+    componentDidMount(){
+        Axios.get(this.url)
+        .then( response => { 
+            const cards = response.data;
+            this.setState({ cards })
+            //console.log(response.data) 
+        })
+    }
+    //
     render(){
         return(
-            <div className="container-md">
-                <Card
-
-                />
+            <div className="album py-5">
+                <div className="container">
+                    <div className="row">
+                        <Card
+                            data = { this.state }
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
